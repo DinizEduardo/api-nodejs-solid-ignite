1a2a4368b707059c486cabbee219e4b8e92851f7
Eduardo Diniz <eduardo.diniz987@gmail.com>
2023-07-06 18:11:20 -0300

    feat: caso de uso de perfil do usuario por id

---
+++ src/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,39 @@
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { hash } from 'bcryptjs'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { GetUserProfile } from './get-user-profile'
+
+let usersRepository: InMemoryUsersRepository
+let sut: GetUserProfile
+
+describe('Authenticate use case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new GetUserProfile(usersRepository)
+  })
+
+  it('should be able to get user profile', async () => {
+    const createdUser = await usersRepository.create({
+      name: 'Eduardo Diniz',
+      email: 'eduardo@diniz.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    const { user } = await sut.execute({
+      userId: createdUser.id,
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+    expect(user.name).toEqual('Eduardo Diniz')
+  })
+
+  it('should be not able to get user profile with invalid id', async () => {
+    await expect(
+      async () =>
+        await sut.execute({
+          userId: 'non-existing-id',
+        }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
