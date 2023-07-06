8f27d08253cfba9a3606d1479f33db2ef3007a7f
Eduardo Diniz <eduardo.diniz987@gmail.com>
2023-07-06 18:38:08 -0300

    feat: usando tdd para validar check ins

--- README.md
+++ README.md
@@ -11,14 +11,14 @@ Gympass style app.
 - [ ] Deve ser possivel o usuario obter o seu historico de check-ins;
 - [ ] Deve ser possivel o usuario buscar academias proximas;
 - [ ] Deve ser possivel o usuario buscar academias pelo nome;
-- [ ] Deve ser possivel o usuario realizar check in em uma academia;
+- [x] Deve ser possivel o usuario realizar check in em uma academia;
 - [ ] Deve ser possivel validar o check-in de um usuario;
 - [ ] Deve ser possivel cadastrar uma academia;
 
 ## RNs (Regras de negocio)
 
 - [x] O usuario não deve poder se cadastrar com um e-mail duplicado;
-- [ ] O usuario não pode fazer 2 check-ins no mesmo dia;
+- [x] O usuario não pode fazer 2 check-ins no mesmo dia;
 - [ ] O usuario não pode fazer check-in se não estiver perto (100m) da academia
 - [ ] O check-in só pode ser validado até 20 minutos após criado;
 - [ ] O check-in só pode ser validado por admnistradores;
--- src/repositories/in-memory/in-memory-check-in-repository.ts
+++ src/repositories/in-memory/in-memory-check-in-repository.ts
@@ -1,10 +1,31 @@
 import { CheckIn, Prisma } from '@prisma/client'
 import { randomUUID } from 'crypto'
+import dayjs from 'dayjs'
 import { CheckInsRepository } from '../check-in-repository'
 
 export class InMemoryCheckInsRepositoryRepository
   implements CheckInsRepository
 {
+  async findByUserIdOnDate(userId: string, date: Date) {
+    const startOfDay = dayjs(date).startOf('date')
+    const endOfDay = dayjs(date).endOf('date')
+
+    const checkInOnSameDate = this.items.find((checkIn) => {
+      const checkInDate = dayjs(checkIn.created_at)
+
+      const isOnSameDate =
+        checkInDate.isAfter(startOfDay) && checkInDate.isBefore(endOfDay)
+
+      return checkIn.user_id === userId && isOnSameDate
+    })
+
+    if (!checkInOnSameDate) {
+      return null
+    }
+
+    return checkInOnSameDate
+  }
+
   public items: CheckIn[] = []
 
   async create(data: Prisma.CheckInUncheckedCreateInput) {
