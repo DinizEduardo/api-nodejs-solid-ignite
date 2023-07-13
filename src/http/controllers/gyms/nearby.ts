f0c706bfad29928c457e39338dc3d7ef599dee2d
Eduardo Diniz <eduardo.diniz987@gmail.com>
2023-07-12 22:57:02 -0300

    feat: criando outros controllers de academia

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,25 @@
+import { makeFetchNeabyGymsUseCase } from '@/use-cases/factories/make-fetch-nearby-gyms-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const featchNeabyQuerySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { latitude, longitude } = featchNeabyQuerySchema.parse(request.body)
+
+  const neabyGymsUseCase = makeFetchNeabyGymsUseCase()
+
+  const { gyms } = await neabyGymsUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return reply.status(200).send({ gyms })
+}
